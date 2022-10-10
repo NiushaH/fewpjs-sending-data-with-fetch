020fa563e459b96dabfa484c4833e0895d721daa
NiushaH <35556570+NiushaH@users.noreply.github.com>
2022-10-09 22:23:19 -0600

    added user form & index.js to post users to DB

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const formData = {
+    name: "Mary Shelley",
+    email: "maryshelley@lady.com"
+  };
+  
+const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+function submitData( formData ) {
+  return fetch( "http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        console.log(object);
+        document.body.innerHTML = object [ "id" ];
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+    });
+  };
